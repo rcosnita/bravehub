947a87b31e763356c74babc354f3d36782f1b6ac
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2017-08-23 00:20:01 +0300

    #29 Create the hbase bravehub schema during production cluster setup.

--- deployment/provisioning/bravehub-bootstrap.sh
+++ deployment/provisioning/bravehub-bootstrap.sh
@@ -3,11 +3,11 @@ set -eo pipefail
 
 . instance-descriptor.sh
 
-export LC_ALL=C
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+sudo dpkg-reconfigure -f noninteractive locales
 sudo apt-get update -y
-sudo apt-get install -y python-pip python-dev build-essential openssl libssl-dev awscli
-sudo pip install --upgrade pip
-sudo pip install virtualenv
+sudo apt-get install -y python-pip python-dev python3-pip python3-dev python3-venv python3-apt build-essential openssl libssl-dev awscli
 
 rm -Rf provisioning
 mkdir provisioning
@@ -18,8 +18,10 @@ aws s3 cp --recursive s3://${STACK_NAME}/provisioning/inventory inventory/
 aws s3 cp --recursive s3://${STACK_NAME}/provisioning/roles roles/
 aws s3 cp s3://${STACK_NAME}/provisioning/${ROLE}.yml .
 
-virtualenv venv
+python3 -m venv venv
 . venv/bin/activate
+pip install --upgrade pip
+cp -R /usr/lib/python3/dist-packages/apt* venv/lib/python3.*/site-packages/
 pip install -r requirements.txt
 
-ansible-playbook -i inventory/local -v ${ROLE}.yml
+ansible-playbook -i inventory/local -v -e "ansible_python_interpreter=$(pwd)/venv/bin/python3" ${ROLE}.yml
