f0fab0d6c1488e149771f1c181577698bbe475a7
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2017-09-30 15:49:43 +0300

    [PREFACTORING] Add support for formating and linting C++ code.

--- cicd/lint-code.sh
+++ cicd/lint-code.sh
@@ -2,4 +2,24 @@
 set -eo pipefail
 
 PROJECT_NAME=${1}
-pylint bravehub_shared ${PROJECT_NAME}/src
+WORKDIR=$(pwd)
+
+if [[ "${PROJECT_NAME}" != "load-balancer" ]]; then
+  pylint bravehub_shared ${PROJECT_NAME}/src
+else
+  cd load-balancer
+  LINT_SRC=$(find src -iname src/build -name "*.cpp" -o -name "*.h")
+
+  # python third-party/depot_tools/cpplint.py --linelength=120 ${LINT_SRC[*]}
+
+  cd ${WORKDIR}
+  docker build -t bravehub/cppcoreguidelines -f cicd/docker/Dockerfile-cppcoreguidelines cicd/docker
+  docker run --rm \
+    --env CPP_LINTING_PATH=/root/load-balancer/src \
+    -v $(pwd)/load-balancer/src:/root/load-balancer/src \
+    -v $(pwd)/load-balancer/third-party:/root/load-balancer/third-party \
+    --workdir /root/load-balancer \
+    bravehub/cppcoreguidelines
+fi
+
+cd ${WORKDIR}
