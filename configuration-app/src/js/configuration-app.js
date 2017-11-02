a2a79b10fa34572a503f33c9cf418e5db784a3c5
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2017-11-02 23:11:15 +0200

    #19 Create new projects from UI.

---
+++ configuration-app/src/js/configuration-app.js
@@ -0,0 +1,47 @@
+class ConfigurationApp extends Polymer.Element {
+  static get is() { return "configuration-app"; }
+
+  static get properties() {
+    return {
+      page: {
+        type: String,
+        reflectToAttribute: true,
+        observer: "_pageChanged",
+      },
+      routeData: Object,
+      subroute: String,
+      rootPath: String,
+    };
+  }
+
+  static get observers() {
+    return [
+      "_routePageChanged(routeData.page)",
+    ];
+  }
+
+  static get defaultView() { return "configuration-manage-projects"; }
+
+  _routePageChanged(page) {
+    this.page = page || ConfigurationApp.defaultView;
+
+    if (!this.$.drawer.persistent) {
+      this.$.drawer.close();
+    }
+  }
+
+  _pageChanged(page) {
+    const resolvedPageUrl = this.resolveUrl(page + ".html");
+    Polymer.importHref(
+        resolvedPageUrl,
+        null,
+        this._showPage404.bind(this),
+        true);
+  }
+
+  _showPage404() {
+    this.page = "configuration-view404";
+  }
+}
+
+window.customElements.define(ConfigurationApp.is, ConfigurationApp);
