357333aba992e387f6b88cef65c46e7c3cabd9ed
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2017-12-06 17:38:11 -0500

    #43 Add api editor.

--- configuration-app/src/js/configuration-api-details.js
+++ configuration-app/src/js/configuration-api-details.js
@@ -1,3 +1,7 @@
+"use strict";
+
+import {Events, UiEvent} from "./events/events.js";
+
 class ConfigurationApiDetails extends Polymer.Element {
   static get properties() {
     return {
@@ -9,6 +13,7 @@ class ConfigurationApiDetails extends Polymer.Element {
       apiModel: {
         type: Object,
         notify: true,
+        observer: "_restoreModel",
       },
       isNew: {
         type: Boolean,
@@ -24,13 +29,48 @@ class ConfigurationApiDetails extends Polymer.Element {
         notify: true,
         observer: "_storeBuildsData",
       },
+      projectModel: Object,
+      configFiles: {
+        type: Object,
+        notify: true,
+        value: { },
+      },
+      droplet: {
+        type: Object,
+        notify: true,
+      },
     };
   }
+
+  constructor() {
+    super();
+    this._projectModel = undefined;
+  }
+
   static get is() { return "configuration-api-details"; }
+
+  createApi() {
+    const projectLocation = this.projectModel.__meta__.location;
+
+    const action = Rx.DOM.ajax({  // eslint-disable-line no-undef
+      url: `${projectLocation}/apis`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.apiModel),
+    });
+
+    action.subscribe(
+      (resp) => this._whenApiCreated(resp),
+      (err) => console.log(err)  // eslint-disable-line no-console
+    );
+  }
 
   _loadDetails(apiSummary) {
     const constants = window.Bravehub.Constants;
     const apiUrl = `${constants.PROJECTS_URL}/${apiSummary.project.id}/apis/${apiSummary.id}`;
+
     Rx.DOM.ajax({  // eslint-disable-line no-undef
       method: "GET",
       url: apiUrl,
@@ -56,8 +96,29 @@ class ConfigurationApiDetails extends Polymer.Element {
     );
   }
 
+  _restoreModel(apiModel) {
+    if (!apiModel) {
+      return;
+    }
+
+    const exposedPorts = (apiModel.exposedPorts || [80]).join(",");
+    const buildsData = this.isNew ? apiModel.build : apiModel.builds;
+    this.buildsData = JSON.stringify(buildsData || {});
+    this._storeBuildsData(this.buildsData);
+
+    this.ports = exposedPorts;
+    this._storePorts(exposedPorts);
+  }
+
   _storeBuildsData(buildsDataStr) {
-    const buildsData = JSON.parse(buildsDataStr);
+    let buildsData = { };
+
+    try {
+      buildsData = JSON.parse(buildsDataStr);
+    } catch (err) {
+      // suppress invalid json error in here. The user might be typing builds data.
+    }
+
     if (this.isNew) {
       this.apiModel.build = buildsData;
     } else {
@@ -67,6 +128,75 @@ class ConfigurationApiDetails extends Polymer.Element {
 
   _storePorts(exposedPorts) {
     this.apiModel.exposedPorts = exposedPorts.replace(" ").split(",").map((p) => parseInt(p));
+  }
+
+  _whenApiCreated(resp) {
+    const apiLocation = resp.xhr.getResponseHeader("Location");
+
+    this._configUploaded = false;
+    this._dropletUploaded = false;
+
+    this._uploadDroplet(apiLocation);
+    this._uploadConfigFiles(apiLocation);
+  }
+
+  _uploadDroplet(apiUrl) {
+    if (!this.droplet) {
+      return;
+    }
+
+    Rx.DOM.ajax({  // eslint-disable-line no-undef
+      url: `${apiUrl}/builds/1`,
+      method: "PUT",
+      body: this.droplet,
+      headers: {
+        "Content-Type": "application/vnd.bravehub.droplet-binary",
+      },
+    }).subscribe(
+      (resp) => {
+        this._dropletUploaded = true;
+        this._whenBinariesUploaded();
+      },
+      (err) => console.log(err)  // eslint-disable-line no-console
+    );
+  }
+
+  _uploadConfigFiles(apiUrl) {
+    Object.keys(this.configFiles).forEach((fName) => {
+      const file = this.configFiles[fName];
+
+      Rx.DOM.ajax({  // eslint-disable-line no-undef
+        url: `${apiUrl}/builds/1/configassets`,
+        method: "POST",
+        body: file,
+        headers: {
+          "Content-Type": "application/vnd.bravehub.configurationasset-binary",
+          "X-ConfigAsset-MountPath": `/${fName}`,
+        },
+      }).subscribe(
+        (resp) => {
+          this._configUploaded = true;
+          this._whenBinariesUploaded();
+        },
+        (err) => console.log(err)  // eslint-disable-line no-console
+      );
+    });
+  }
+
+  _whenBinariesUploaded() {
+    if (!this._configUploaded || !this._dropletUploaded) {
+      return;
+    }
+
+    this._configUploaded = false;
+    this._dropletUploaded = false;
+
+    alert("API created successfully ...");
+
+    const evtName = Events.gestures.CLOSE_EDITOR;
+    this.messageBus.emit(evtName, new UiEvent(evtName, {
+      "node": this,
+    }));
   }
 }
 
---
+++ configuration-app/src/js/editor/api-editor.js
@@ -0,0 +1,44 @@
+"use strict";
+
+import Editor from "./editor.js";
+import {Events, ModelEvent} from "../events/events.js";
+
+export default class ApiEditor extends Editor {
+  constructor(opts) {
+    super();
+    this._component = undefined;
+    this._createApiComponent();
+  }
+
+  get domElement() { return this._component; }
+
+  restore(opts) {
+    const model = opts.model;
+
+    this._component.apiModel = model;
+    this._component.messageBus = opts.messageBus;
+
+    this._fetchCurrentProject(opts.messageBus);
+  }
+
+  _createApiComponent() {
+    this._component = document.createElement("configuration-api-details");
+    this._component.style.width = "300px";
+    this._component.style.height = "300px";
+    this._component.style.visibility = "hidden";
+    this._component.style.position = "absolute";
+    this._component.apiModel = { };
+    this._component.isNew = true;
+  }
+
+  _fetchCurrentProject(messageBus) {
+    const evtName = Events.models.GET_CURRENT_PROJECT;
+    const evtNameResponse = Events.models.GET_CURRENT_PROJECT_LOADED;
+
+    messageBus.on(evtNameResponse, (evt) => {
+      this._component.projectModel = evt.model;
+    });
+
+    messageBus.emit(evtName, new ModelEvent(evtName));
+  }
+}
