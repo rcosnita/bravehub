d69bcace3e0711b24208551472d492cd019ef3ba
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2017-12-06 17:37:35 -0500

    #43 Add project editor.

---
+++ configuration-app/src/js/editor/project-editor.js
@@ -0,0 +1,28 @@
+"use strict";
+
+import Editor from "./editor.js";
+
+export default class ApiEditor extends Editor {
+  constructor(opts) {
+    super();
+    this._component = undefined;
+    this._createProjectComponent();
+  }
+
+  get domElement() { return this._component; }
+
+  restore(opts) {
+    const model = opts.model;
+    this._component.projectModel = model;
+    this._component.messageBus = opts.messageBus;
+  }
+
+  _createProjectComponent() {
+    this._component = document.createElement("configuration-project-details");
+    this._component.style.width = "300px";
+    this._component.style.height = "300px";
+    this._component.style.visibility = "hidden";
+    this._component.style.position = "absolute";
+    this._component.projectModel = { };
+  }
+}
