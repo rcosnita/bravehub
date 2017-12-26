fb81a9175e000cbefa8cd94980aa3d02183ac29e
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2017-12-27 00:56:26 +0200

    #54 Serve platform apis through the router layer.

--- configuration-app/src/js/constants.js
+++ configuration-app/src/js/constants.js
@@ -4,7 +4,7 @@ if (!window.Bravehub) {
 
 if (!window.Bravehub.Constants) {
   window.Bravehub.Constants = (function() {
-    const BASE_URL = "http://localhost:5000";
+    const BASE_URL = "http://configuration-api.api.bravehub-dev.com";
     const PROJECTS_URL = `${BASE_URL}/v0.1/projects`;
 
     return {
