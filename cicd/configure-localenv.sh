ac07b07199960d283c5315c9fcea88e0506affe6
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2018-01-02 16:58:56 +0200

    #74 Add support for visual studio code.

---
+++ cicd/configure-localenv.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+WORKDIR=$(pwd)
+
+function create_venv {
+  VENV_PATH=${1}
+  SRC_PATH=${2}
+  SHARED_PATH=${WORKDIR}/bravehub_shared
+
+  pushd ${SRC_PATH}
+  rm -Rf bravehub_shared
+  ln -s ${SHARED_PATH} ${SRC_PATH}/bravehub_shared
+  popd
+
+  python3 -m venv ${VENV_PATH}
+  . ${VENV_PATH}/bin/activate
+  pip install -r requirements.txt
+  pip install -r ${SRC_PATH}/bravehub_shared/requirements.txt
+  pip install pylint
+  deactivate
+}
+
+pushd bravehub_shared
+echo "Configuring bravehub_shared"
+create_venv $(pwd)/bravehub-shared-env $(pwd)
+popd
+pwd
+
+pushd configuration-api
+echo "Configuring configuration-api"
+create_venv $(pwd)/configuration-api-env $(pwd)/src
+popd
+
+pushd ./logging-api
+echo "Configuring logging-api"
+create_venv $(pwd)/logging-api-env $(pwd)/src
+popd
+
+pushd ./provisioning-api
+echo "Configuring provisioning-api"
+create_venv $(pwd)/provisioning-api-env $(pwd)/src
+popd
---
+++ docs/ide/visualstudio-code.md
@@ -0,0 +1,11 @@
+In order to accelerate the development process we support out of the box integration with **Visual Studio Code IDE**.
+
+## Manual steps
+
+In order to be able to trigger all operations from Visual Studio Code, you must execute the following script:
+
+```bash
+sh cicd/configure-localenv.sh
+```
+
+Now, you can easily open **bravehub.code-workspace** into Visual Studio Code.
