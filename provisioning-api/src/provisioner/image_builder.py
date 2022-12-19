eacb9bbb8d7e5dc31435f8e231986b86abc71026
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-19 13:27:09 +0000

    Adding tarfile member sanitization to extractall()

--- provisioning-api/src/provisioner/image_builder.py
+++ provisioning-api/src/provisioner/image_builder.py
@@ -125,7 +125,26 @@ class DockerImageBuilder(ImageBuilder):  # pylint: disable=too-few-public-method
 
     droplet_dest = self._file_system.absolute_path(droplet_dest)
     with tarfile.open(droplet_dest) as archive:
-      archive.extractall(self._file_system.absolute_path(image_folder))
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
+          
+      
+      safe_extract(archive, self._file_system.absolute_path(image_folder))
 
     os.remove(droplet_dest)
 
